a8d167b91737b74841052109a1ee7a9ddd327caf
PRINCE3718 <112079179+PRINCE3718@users.noreply.github.com>
2023-04-22 15:13:18 +0530

    Add files via upload

---
+++ ASSESMENT/Assessment/css/main.js
@@ -0,0 +1,8 @@
+function si()
+{
+    var e=document.getElementById("e").Value;
+    var r=document.getElementById("r").Value;
+    var f=document.getElementById("f").Value;
+    // var t=e*r*f*/1000;
+    document.getElementById("demo").innerHTML="simple:"+t;
+}
